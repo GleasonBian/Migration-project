deb2ca1a3806b1f6a71afb36969bc967898dab27
Gleason <bianliuzhu@gmail.com>
2019-05-06 18:19:30 +0800

    配置webpack

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,25 @@
 const path = require('path')
-// 路径
-const resolve = (dir) => {
+function resolve (dir) {
   return path.join(__dirname, '..', dir)
-}
-let srcAds = {
-  'components': resolve('src/components'),
-  'bizcomponents': resolve('src/bizcomponents'),
-  'customtemp': resolve('src/components/customtemp'),
-  'common': resolve('src/common'),
-  'static': resolve('src/static'),
-  'vue$': 'vue/dist/vue.esm.js',
-  'view': resolve('src/view'),
-  'store': resolve('src/store'),
-  'base': resolve('base'),
-  'api': resolve('src/api'),
-  '@': resolve('src')
 }
 module.exports = {
   configureWebpack: {
     entry: './src/view/index/main.js',
     resolve: {
-      alias:srcAds
-    }
+      extensions: ['.js', '.vue', '.json'],
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js',
+        '@': resolve('src'),
+        'components': resolve('src/components'),
+        'bizcomponents': resolve('src/bizcomponents'),
+        'customtemp': resolve('src/components/customtemp'),
+        'common': resolve('src/common'),
+        'static': resolve('src/static'),
+        'view': resolve('src/view'),
+        'store': resolve('src/store'),
+        'base': resolve('base'),
+        'api': resolve('src/api')
+      }
+    },
   }
 }
